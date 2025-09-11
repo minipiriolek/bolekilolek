9eca75d891e408d1a853ea0248622ed2275988dd
olo <aleksander.malicki@cyfroweszkoly.pl>
2025-09-11 11:16:51 +0200

    don

---
+++ test1/MainWindow.axaml.cs
@@ -0,0 +1,31 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace test1;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnRemoveTaskClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void OnShowTasksForDateClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void OnAddTaskClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    
+    
+    
+}
