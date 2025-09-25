2122391255f0765f371abe5b736f3147c156540f
olo <aleksander.malicki@cyfroweszkoly.pl>
2025-09-25 10:51:39 +0200

    don2

--- test1/MainWindow.axaml.cs
+++ test1/MainWindow.axaml.cs
@@ -1,31 +1,90 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 
-namespace test1;
-
-public partial class MainWindow : Window
+namespace test1
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private ObservableCollection<TaskItem> tasks = new ObservableCollection<TaskItem>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TaskListBox.ItemsSource = tasks;
+            CharacterComboBox.SelectedIndex = 0;
+            UpdateCharacterImage();
+        }
+
+        private void UpdateCharacterImage() //nie dziala
+        {
+            if (CharacterComboBox.SelectedItem is ComboBoxItem item)
+            {
+                var name = item.Content?.ToString() ?? "";
+                var uri = $"avares://test1/Assets/{(name == "Bolek" ? "bolek.png" : "lolek.png")}";
+                try { CharacterImage.Source = new Bitmap(uri); } catch { CharacterImage.Source = null; }
+            }
+        }
+
+        private void OnCharacterChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            UpdateCharacterImage();
+        }
+
+        private void OnAddTaskClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var name = TaskNameBox.Text ?? "";
+            if (string.IsNullOrWhiteSpace(name)) return;
+
+            var character = (CharacterComboBox.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "???";
+            var priority = LowPriority.IsChecked == true ? "łatwe" :
+                           NormalPriority.IsChecked == true ? "średnie" :
+                           HighPriority.IsChecked == true ? "trudne" : "zwykłe";
+            var extras = "";
+            if (OutdoorCheckBox.IsChecked == true) extras += " 🌳";
+            if (EquipmentCheckBox.IsChecked == true) extras += " 🔧";
+            if (FriendsCheckBox.IsChecked == true) extras += " 🤝";
+            var date = TaskCalendar.SelectedDate ?? DateTime.Today;
+
+            var task = new TaskItem
+            {
+                Name = name,
+                Character = character,
+                Priority = priority,
+                Extras = extras,
+                Date = date.Date
+            };
+
+            tasks.Add(task);
+            TaskNameBox.Text = "";
+        }
+
+        private void OnRemoveTaskClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (TaskListBox.SelectedItem is TaskItem selected)
+            {
+                tasks.Remove(selected);
+            }
+        }
+
+        private void OnShowTasksForDateClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var date = TaskCalendar.SelectedDate ?? DateTime.Today;
+            var summary = new SummaryWindow(tasks, date.Date);
+            summary.Show();
+        }
     }
 
-    private void OnRemoveTaskClick(object? sender, RoutedEventArgs e)
+    public class TaskItem
     {
-        throw new System.NotImplementedException();
-    }
+        public string Name { get; set; } = "";
+        public string Character { get; set; } = "";
+        public string Priority { get; set; } = "";
+        public string Extras { get; set; } = "";
+        public DateTime Date { get; set; }
 
-    private void OnShowTasksForDateClick(object? sender, RoutedEventArgs e)
-    {
-        throw new System.NotImplementedException();
+        public override string ToString() => $"{Character} – {Name} [{Priority}] {Extras}";
     }
-
-    private void OnAddTaskClick(object? sender, RoutedEventArgs e)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    
-    
-    
 }
--- test1/obj/Debug/net9.0/test1.AssemblyInfo.cs
+++ test1/obj/Debug/net9.0/test1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eca75d891e408d1a853ea0248622ed2275988dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("test1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
